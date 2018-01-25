68025030e56bb21880aa7eb6443f3e2b48e3dcfe
Han Yan <yhaddint@ucla.edu>
2018-01-25 11:36:55 -0800

    Updated blocker_tagging_inter (06/23/2014)

--- BlockerTagging_inter.m
+++ BlockerTagging_inter.m
@@ -1,26 +1,26 @@
-
 %
-%  06/20/2014
-%  We keep M = 16 and N =10, blocker_pow_pool is not a matrix 
+%  06/21/2014
+%  watch statistics of each stage
+%  06/22/2014
+%  actually, this method doesn't help to surpress interference a lot.
 
 clear;clc;clf;close all
 warning off
 
-trueCal=0;
-
 symbols=5e4;
-upsam=8;
+upsam=500;
 
 
-N=5;
-M=40;
+N=200;
+M=20;
 
 %  tune power of each blockers
 blocker_num=3;
-int_power_pool=[1 5 10 20 50 100]/10';
+int_power_pool=[5 10 20 50 100 200]/100';
+%int_power_pool=0.1';
 
 blocker_pow_pool=zeros(length(int_power_pool),blocker_num);
-blocker_pow_pool(:,1)=10;
+blocker_pow_pool(:,1)=1;
 blocker_pow_pool(:,2)=int_power_pool;
 blocker_pow_pool(:,3)=int_power_pool;
 
@@ -29,7 +29,7 @@ do_est=0;
 do_det=1;
 
 % number of data accumulated to watch histogram.
-height=625;
+height=200;
 
 % initialization of statistics store vector for int_power_pool
 rslt_matrix_h0=zeros(length(int_power_pool),height);
@@ -39,11 +39,7 @@ mark=zeros(1,length(int_power_pool));
 
 L=4e5;
 
-
-for int_power_index=1:length(int_power_pool)
-    
-    %  QPSK Blocker generator with power scaled    
-    for ii=1:3
+for ii=1:3
     clear data
     hmod = modem.pskmod('M', 4, 'InputType', 'integer');
     hdesign  = fdesign.pulseshaping(upsam,'Square Root Raised Cosine');
@@ -53,37 +49,58 @@ for int_power_index=1:length(int_power_pool)
     data = upsample(data,upsam);
     sig_bb(:,ii) = conv(data,hpulse.Numerator);
     sig_bb(:,ii)=sig_bb(:,ii)/(sqrt(mean(abs(sig_bb(:,ii)).^2)));
-    blocker(ii,:)=real(sig_bb(1:L,1))./mean(real(sig_bb(:,1)).^2)*blocker_pow_pool(int_power_index,ii);
-    end
+end
 
-
-    %  calibration seq, we use shifted version to approximate
-    if trueCal
-    cal0=PNgenerator_v1(L+2);
-    cal(1,:)=cal0(1:end-2);
-    cal(2,:)=cal0(2:end-1);
-    cal(3,:)=cal0(3:end);
-    end
+for int_power_index=1:length(int_power_pool)
     
-    %  Another version of cal
-    if trueCal==0
+    %  QPSK Blocker generator with power scaled    
+    for ii=1:3
+        blocker(ii,:)=real(sig_bb(1:L,1))./mean(real(sig_bb(:,1)).^2)*blocker_pow_pool(int_power_index,ii);
+    end
+
+
     cal0=PNgenerator_v1(M*N*4);
-    cal(1,:)=kron(ones(2000,1),cal0(M*N*3+1:end));
-    cal(2,:)=kron(ones(2000,1),cal0(M*N+1:2*M*N));
-    cal(3,:)=kron(ones(2000,1),cal0(M*N*2+1:3*M*N));
+    cal(1,:)=kron(ones(1e3*M/N,1),cal0(M*N*3+1:end));
+    cal(2,:)=kron(ones(1e3*M/N,1),cal0(M*N+1:2*M*N));
+    cal(3,:)=kron(ones(1e3*M/N,1),cal0(M*N*2+1:3*M*N));
     
-    end
 
     %  calculate statistics
-    stat_h0=0.5*(cal(1,:)*2+(blocker(2,:).^2+blocker(3,:).^2).*cal(1,:))+blocker(3,:).*cal(1,:).*cal(3,:)+blocker(2,:).*cal(1,:).*cal(2,:);
+    stat_h0=0.5*(cal(1,:)*2+(blocker(2,:).^2+blocker(3,:).^2).*cal(1,:))+...
+        blocker(3,:).*cal(1,:).*cal(3,:)+blocker(2,:).*cal(1,:).*cal(2,:);
     stat_h1=0.5*(cal(1,:)+blocker(1,:).^2.*cal(1,:))+blocker(1,:)+stat_h0;
+    temp1=0.5*(cal(1,:)+blocker(1,:).^2.*cal(1,:));
+    temp2=0.5*(cal(1,:)*2+(blocker(2,:).^2+blocker(3,:).^2).*cal(1,:));
+    temp3=blocker(3,:).*cal(1,:).*cal(3,:)+blocker(2,:).*cal(1,:).*cal(2,:);
+    temp4=blocker(1,:);
     
-
     for ii=1:fix(L/N/2)
-        rslt_h0(ii)=abs(sum(stat_h0((ii-1)*N+1:ii*N))/N);
-        rslt_h1(ii)=abs(sum(stat_h1((ii-1)*N+1:ii*N))/N);
+        temp1_sum(ii)=sum(temp1((ii-1)*N+1:ii*N)/N);
+        temp2_sum(ii)=sum(temp2((ii-1)*N+1:ii*N)/N);
+        temp3_sum(ii)=sum(temp3((ii-1)*N+1:ii*N)/N);
+        temp4_sum(ii)=sum(temp4((ii-1)*N+1:ii*N)/N);
+        rslt_h0(ii)=abs(sum(stat_h0((ii-1)*N+1:ii*N)/N));
+        rslt_h1(ii)=abs(sum(stat_h1((ii-1)*N+1:ii*N)/N));
     end
-
+    
+%     %%
+%     figure
+%     subplot(2,2,1)
+%     stem(temp1(100:200));
+%     subplot(2,2,2)
+%     stem(temp2(100:200));
+%     subplot(2,2,3)
+%     stem(temp3(100:200));
+%     subplot(2,2,4)
+%     stem(temp4(100:200));
+    %%
+    
+    S_sta_h0(:,int_power_index)=rslt_h0;
+    S_sta_h1(:,int_power_index)=rslt_h1;
+    S_mean_h0(int_power_index)=mean(S_sta_h0(:,int_power_index));
+    S_mean_h1(int_power_index)=mean(S_sta_h1(:,int_power_index));
+    S_var_h0(int_power_index)=var(S_sta_h0(:,int_power_index));
+    S_var_h1(int_power_index)=var(S_sta_h1(:,int_power_index));
     
     l_need=fix(length(rslt_h0)/M);
     if M==1
@@ -148,14 +165,23 @@ end
 
 figure(1)
 title('ROC curve');
-legend('int = 1','int = 5','int = 10','int = 20','int = 50','int = 100')
+legend('int = 0.0625','int = 0.125','int = 0.25','int = 0.5','int = 1','int = 2')
 
 end
 
 
 
 
-%% estimation of blocker power
-if do_est
-
+%% hist of stat S
+figure(10)
+for ii=1:length(int_power_pool)
+subplot(2,3,ii)
+hist(S_sta_h0(:,ii));hold on
 end
+
+figure(11)
+for ii=1:length(int_power_pool)
+subplot(2,3,ii)
+hist(S_sta_h1(:,ii));hold on
+end
+
