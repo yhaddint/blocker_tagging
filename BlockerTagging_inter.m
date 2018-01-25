1efafc0527f16475b976d908741604672ef36357
Han Yan <yhaddint@ucla.edu>
2018-01-25 11:34:57 -0800

    Updated blocker_tagging_inter (06/22/2014)

--- BlockerTagging_inter.m
+++ BlockerTagging_inter.m
@@ -1,38 +1,48 @@
 
-%  06/17/2014
-%  Interference in blocker tagging is clearer in concept. In this
-%  simulation it is added.
-% 
+%
+%  06/20/2014
+%  We keep M = 16 and N =10, blocker_pow_pool is not a matrix 
 
 clear;clc;clf;close all
 warning off
 
+trueCal=0;
+
 symbols=5e4;
 upsam=8;
 
-% tune parameter M, N and relative power of blockers v.s. PN sequences
-Npool=[5 10 20 40 80];
-Mpool=[4 8 16 32];
-sig_pow_pool=[1 2 4 8 16];
-blocker_pow_pool=[20 100 20];
-noisepower=0;
+
+N=5;
+M=40;
+
+%  tune power of each blockers
+blocker_num=3;
+int_power_pool=[1 5 10 20 50 100]/10';
+
+blocker_pow_pool=zeros(length(int_power_pool),blocker_num);
+blocker_pow_pool(:,1)=10;
+blocker_pow_pool(:,2)=int_power_pool;
+blocker_pow_pool(:,3)=int_power_pool;
 
 % determined whether to apply estimation of power and detection
 do_est=0;
 do_det=1;
 
 % number of data accumulated to watch histogram.
-height=5e3;
+height=625;
 
-% initialization of data store matrix for M, N pair
-rslt_matrix_h0=zeros(length(Npool),length(sig_pow_pool),height);
-rslt_matrix_h1=zeros(length(Npool),length(sig_pow_pool),height);
+% initialization of statistics store vector for int_power_pool
+rslt_matrix_h0=zeros(length(int_power_pool),height);
+rslt_matrix_h1=zeros(length(int_power_pool),height);
 
-mark=zeros(length(Npool),length(Mpool));   
+mark=zeros(1,length(int_power_pool));   
 
 L=4e5;
 
-for runtime=1:2
+
+for int_power_index=1:length(int_power_pool)
+    
+    %  QPSK Blocker generator with power scaled    
     for ii=1:3
     clear data
     hmod = modem.pskmod('M', 4, 'InputType', 'integer');
@@ -41,41 +51,37 @@ for runtime=1:2
     data = randi(4,symbols,1)-1;
     data = modulate(hmod, data);
     data = upsample(data,upsam);
-    data = conv(data,hpulse.Numerator);
-
-    sig_bb(:,ii)=data;
+    sig_bb(:,ii) = conv(data,hpulse.Numerator);
     sig_bb(:,ii)=sig_bb(:,ii)/(sqrt(mean(abs(sig_bb(:,ii)).^2)));
-    
-    blocker(ii,:)=real(sig_bb(1:L,1))./mean(real(sig_bb(:,1)).^2)*blocker_pow_pool(ii);
-   
+    blocker(ii,:)=real(sig_bb(1:L,1))./mean(real(sig_bb(:,1)).^2)*blocker_pow_pool(int_power_index,ii);
     end
 
 
-    %% signal processing
-    
+    %  calibration seq, we use shifted version to approximate
+    if trueCal
     cal0=PNgenerator_v1(L+2);
     cal(1,:)=cal0(1:end-2);
     cal(2,:)=cal0(2:end-1);
     cal(3,:)=cal0(3:end);
-
+    end
     
+    %  Another version of cal
+    if trueCal==0
+    cal0=PNgenerator_v1(M*N*4);
+    cal(1,:)=kron(ones(2000,1),cal0(M*N*3+1:end));
+    cal(2,:)=kron(ones(2000,1),cal0(M*N+1:2*M*N));
+    cal(3,:)=kron(ones(2000,1),cal0(M*N*2+1:3*M*N));
+    
+    end
+
+    %  calculate statistics
     stat_h0=0.5*(cal(1,:)*2+(blocker(2,:).^2+blocker(3,:).^2).*cal(1,:))+blocker(3,:).*cal(1,:).*cal(3,:)+blocker(2,:).*cal(1,:).*cal(2,:);
     stat_h1=0.5*(cal(1,:)+blocker(1,:).^2.*cal(1,:))+blocker(1,:)+stat_h0;
-
     
-    for nn=1:length(Npool)
-    for mm=1:length(Mpool)
-        clc
-        display(['Run number',num2str(runtime)]);
-        display(['N = ',num2str(Npool(nn)) ', M = ',num2str(Mpool(mm))]);
-    clear rslt_cancel rslt_noncancel rslt_PN ii i
-    
-    N=Npool(nn);
-    M=Mpool(mm);
 
     for ii=1:fix(L/N/2)
-        rslt_h0(ii)=abs(mean(stat_h0((ii-1)*N+1:ii*N)));
-        rslt_h1(ii)=abs(mean(stat_h1((ii-1)*N+1:ii*N)));
+        rslt_h0(ii)=abs(sum(stat_h0((ii-1)*N+1:ii*N))/N);
+        rslt_h1(ii)=abs(sum(stat_h1((ii-1)*N+1:ii*N))/N);
     end
 
     
@@ -89,139 +95,67 @@ for runtime=1:2
     end
     
     
-    if mark(nn,mm)<height
+    if mark(int_power_index)<height
     % it means we should update new data into it
-        if mark(nn,mm)+length(temp_h0)<=height
-            rslt_matrix_h0(nn,mm,mark(nn,mm)+1:mark(nn,mm)+length(temp_h0))=temp_h0;
-            rslt_matrix_h1(nn,mm,mark(nn,mm)+1:mark(nn,mm)+length(temp_h1))=temp_h1;
-            mark(nn,mm)=mark(nn,mm)+length(temp_h0);
+        if mark(int_power_index)+length(temp_h0)<=height
+            rslt_matrix_h0(int_power_index,mark(int_power_index)+1:mark(int_power_index)+length(temp_h0))=temp_h0;
+            rslt_matrix_h1(int_power_index,mark(int_power_index)+1:mark(int_power_index)+length(temp_h1))=temp_h1;
+            mark(int_power_index)=mark(int_power_index)+length(temp_h0);
         else
-            rslt_matrix_h0(nn,mm,mark(nn,mm)+1:height)=temp_h0(1:height-mark(nn,mm));
-            rslt_matrix_h1(nn,mm,mark(nn,mm)+1:height)=temp_h1(1:height-mark(nn,mm));
-            mark(nn,mm)=height;
+            rslt_matrix_h0(int_power_index,mark(int_power_index)+1:height)=temp_h0(1:height-mark(int_power_index));
+            rslt_matrix_h1(int_power_index,mark(int_power_index)+1:height)=temp_h1(1:height-mark(int_power_index));
+            mark(int_power_index)=height;
         end
     end
-    % figure
-    % hist(rslt2,20)
-    % title('sequencially pick')
-
-
     
-    % clear ii
-    % for ii=1:fix(N/M)
-    %     rslt4(ii)=mean(cal((ii-1)*M+1:ii*M).^3);
-    % end
-
-    % %% sequencially pick illustration
-    % figure
-    % plot(real(sig_bb(1:500,1)),'c','linewidth',2);
-    % hold
-    % plot(100:150,real(sig_bb(100:150,1)),'kx');
-    % grid
-    % 
-    % 
-    % %% randomly pick illustration
-    % index=randi(500,1,50);
-    % figure
-    % plot(real(sig_bb(1:500,1)),'c','linewidth',2);
-    % hold
-    % plot(sort(index),real(sig_bb(sort(index))),'kx');
-    % grid
-
-
-    %[H,gaussfit(mm,nn,:)]=chi2gof(rslt_matrix(mm,nn,:));
-    end
-    end
+    
 end
-    % Before next signal power, accumulate needed data
-    for nn=1:length(Npool)
-        h1_stat_mean(mm,nn)=mean(squeeze(rslt_matrix_h1(nn,mm,:)));
-    end
+
 
 %% detection and plot
 % use do_det to control this section
 if do_det
 color=['b  ';'r  ';'c  ';'k  ';'r--';'c--'];
-%th_pool=-log(linspace(0,1,5000));
-th_pool=linspace(0,1e4,4000);
-for nn=1:length(Npool)
-    subplot(2,3,nn)
-    for mm=1:length(Mpool)
-        temp_h0=squeeze(rslt_matrix_h0(nn,mm,:));
-        temp_h1=squeeze(rslt_matrix_h1(nn,mm,:));
-        for th_index=1:length(th_pool)
-            th=th_pool(th_index);
-            prob_fa(th_index)=sum(temp_h0>th)/mark(nn,mm);
-            prob_d(th_index)=sum(temp_h1>th)/mark(nn,mm);
-        end
-        figure(1)
-        plot(prob_fa,prob_d,color(mm,:),'linewidth',2);hold on
-        grid on
-        
-        if Npool(nn)*Mpool(mm)==160
-            figure(99)
-            plot(prob_fa,prob_d,color(mm,:),'linewidth',2);hold on
-        end
+    
+for int_power_index=1:length(int_power_pool)
+    
+    
+    temp_h0=squeeze(rslt_matrix_h0(int_power_index,:));
+    temp_h1=squeeze(rslt_matrix_h1(int_power_index,:));
+    th_pool=linspace(0,max(temp_h0),4000);
+
+    for th_index=1:length(th_pool)
+        th=th_pool(th_index);
+        prob_fa(th_index)=sum(temp_h0>th)/mark(int_power_index);
+        prob_d(th_index)=sum(temp_h1>th)/mark(int_power_index);
     end
+
     figure(1)
-    title(['N = ' num2str(Npool(nn))])
-    legend('M = 4','M = 8','M = 16','M = 32')
+    plot(prob_fa,prob_d,color(int_power_index,:),'linewidth',2);hold on
+    grid on
+    
+    clearvars temp_h0 temp_h1
+    
+    figure(2)
+    subplot(2,3,int_power_index)
+    temp_h0=squeeze(rslt_matrix_h0(int_power_index,:))';
+    temp_h1=squeeze(rslt_matrix_h1(int_power_index,:))';
+    hist_axis=linspace(0,fix(max(temp_h1)),10);
+    hist([temp_h0 temp_h1],hist_axis);hold on
+    title(['Int = ' num2str(int_power_pool(int_power_index))]);
 
 end
 
-figure(99)
-title('With Same Sample Number')
-legend('N=5, M=32','N=10, M=16','N=20, M=8','N=40, M=4');
-grid on
-
-% temp=fix(10*log10(sig_pow));
-% saveas(figure(1),['ratio' num2str(temp) 'sweep.eps'],'epsc');
-% saveas(figure(99),['ratio' num2str(temp) 'sample.eps'],'epsc');
-
-% %% hist of noncancelling term
-% figure
-% for ii=1:4
-% subplot(2,2,ii)
-% hist(squeeze(rslt_matrix(ii,1,:)))
-% title(['N=',num2str(Npool(ii))]);
-% end
-
-% plot hist of h0
-figure
-for mm=1:length(Mpool)
-subplot(2,2,mm);
-hist(squeeze(rslt_matrix_h0(mm,1,:)),10);
-title(['H0 N = 5, M=' num2str(Mpool(mm))]);
-end
-
-figure
-for mm=1:length(Mpool)
-subplot(2,2,mm);
-hist(squeeze(rslt_matrix_h1(mm,1,:)),10);
-title(['H1 N = 5, M=' num2str(Mpool(mm))]);
-end
+figure(1)
+title('ROC curve');
+legend('int = 1','int = 5','int = 10','int = 20','int = 50','int = 100')
 
 end
+
+
+
 
 %% estimation of blocker power
 if do_est
-    AP=1;
-    color=['b-o';'r-o';'c-o';'k-o';'m-o'];
-    color_AP=['b--';'r--';'c--';'k--';'m--'];
-    figure(99)
-    for mm=1:length(sig_pow_pool)
-        semilogy(Npool,h1_stat_mean(mm,:),color(mm,:),'linewidth',2);
-        hold on
-    end
-    grid on
-    legend('S/PN = 0dB','S/PN = 3dB','S/PN = 6dB','S/PN = 9dB','S/PN = 12dB')
-    xlabel('N')
-    ylabel('statistic value')
-    if AP
-        figure(99)
-        for mm=1:length(sig_pow_pool)
-            semilogy(Npool,ones(1,length(Npool))*sig_pow_pool(mm),color_AP(mm,:),'linewidth',2);
-            hold on
-        end
-    end
+
 end
