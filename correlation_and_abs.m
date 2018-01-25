0f6802df3a0fedf3685c4f77e059b99834f5c21c
Han Yan <yhaddint@ucla.edu>
2018-01-25 13:56:18 -0800

    Updated function for taking correlation (despead aliased signal) and use absolute law for detection statistics

--- correlation_and_abs.m
+++ correlation_and_abs.m
@@ -1,20 +1,21 @@
-function [ sig_MNsorted ] = tagging_v1( sig,cal,N,M )
-%TAGGING_V1 Summary of this function goes here
-%   Detailed explanation goes here
+function [ sig_MNave ] = tagging_v2( sig,cal,N,M )
+
+%  version 3 is energy detection while version 2 is absolute value
+%  detection
 
 L=length(sig);
 
 sig_corr=sig.*cal;
 
-for ii=1:fix(L/N/2)
-    sig_Nave(ii)=abs(sum(sig_corr((ii-1)*N+1:ii*N)/N));
-end
+%  N average
+ll_need=L/N;
+sig_Nave=abs(mean(reshape(sig_corr,N,ll_need)));
 
+%  M average
 l_need=fix(length(sig_Nave)/M);
-
 sig_MNave=mean(reshape(sig_Nave(1:M*l_need),M,l_need));
 
-sig_MNsorted=sort(sig_MNave);
+
 % TH_standard=sorted_standard(fix(length(sorted_standard)*0.2));  
 
 end
