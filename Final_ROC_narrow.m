5af54f69c3193f80871af860f3d296486d8f9ae4
Han Yan <yhaddint@ucla.edu>
2018-01-25 17:04:04 -0800

    Updated ROC curve script

--- Final_ROC_narrow.m
+++ Final_ROC_narrow.m
@@ -1,15 +1,15 @@
-%  09/24/2014
-%  adapt to Narrow CAL model. Fix problem on Power/Amplitude
-%  Now complex blockers are considered.
+%  10/02/2014
+%  15 BLKs with Power uniformly from [-50,-30]dBm. BW from 100KHz to 1MHz
+%  Pd=0.9, Pfa=0.1 get
 
-%clearvars -Pfa_eq -Pd_eq;
+clear;
 clc;
 warning off
 
 worstcase=0;
 
 % Independent repeating experiment.
-runtimes=2e2;
+runtimes=1e2;
 
 %  parameter M and N setting. N is also length of CAL Sequences
 N=15;
@@ -21,24 +21,19 @@ SampleNumberAve=N*downSampleTo;
 P=M*SampleNumberAve;
 
 %  How many BLK we want to tag simutaneously
-tag_num=20;
+BLK_num=15;
 
 %  How many active BLK existing. To get reasonable number of different
 %  envelope, we generate 3x of them.
-if worstcase
-    BLK_num=6;
-else
-    BLK_num=14;
-end
 
-BLKcan_num=20;
+BLKcan_num=50;
 
 % We assume simplified model hold (Strong CAL).
 lowerThanPNdB=40;
 lowerThanPN=10^(lowerThanPNdB/10);
 
 %  Define length of BLK we want.
-stat_num=40;
+stat_num=20;
 L=M*SampleNumberAve*stat_num;
 
 %  Averaging Time = xT
@@ -47,8 +42,8 @@ L=M*SampleNumberAve*stat_num;
 %  Normalized BLK generation. 
 %  CAL BW/ BLK BW = 100.
 for ii=1:BLKcan_num
-    upsam=SampleNumberAve*2;
-    symbols=fix(L*2/upsam);
+    upsam=randi([SampleNumberAve*2 SampleNumberAve*20]);
+    symbols=max(fix(L*2/upsam),10);
     
     clearvars data temp_data
     hmod = modem.pskmod('M', 4, 'InputType', 'integer');
@@ -62,34 +57,16 @@ for ii=1:BLKcan_num
 end
 
 %% calibration sequences
-cal0 = PN_generator_downsample(N,N,downSampleTo);
-CAL = LowerRate(cal0, 1, P);
+cal0=PNgenerator_v5(N,N,downSampleTo);
+CAL=LowerRate_v2(cal0,P);
 
-%% initialization of tagging tables
 
-TH_num=100;
-tagged_table_h0=zeros(TH_num,stat_num,runtimes);
-tagged_table_h1=zeros(TH_num,stat_num,runtimes);
+%%
+BLK_pow_pool_db=rand(BLK_num,runtimes)*20-50;
+BLK_amp_pool=sqrt(10.^(BLK_pow_pool_db./10));
 
 
 %% Interference Generation and Tagging
-
-if worstcase
-    BLK_pow_pool_dB=ones(BLK_num,BLK_num-1)*(-40);
-    BLK_pow_pool_dB(1,:)=ones(1,BLK_num-1)*(-10);
-    for bb=1:BLK_num-1
-        BLK_pow_pool_dB(2:bb+1,bb)=ones(bb,1)*(10);
-    end
-else
-    BLK_pow_pool_dB=ones(BLK_num,(BLK_num-2)/4+1)*(-40);
-    BLK_pow_pool_dB(1,:)=ones(1,(BLK_num-2)/4+1)*(-10);
-    for bb=2:BLK_num
-        BLK_pow_pool_dB(bb,fix((bb+5)/4):end)=3;
-    end
-end
-
-
-BLK_pow_pool=(10.^(BLK_pow_pool_dB./10))./lowerThanPN;
 
 for runindex=1:runtimes
 
@@ -109,84 +86,86 @@ for runindex=1:runtimes
     end
     
     %  Power Scaling
-    storedbb_h0=zeros(M*SampleNumberAve,BLK_num);
-    storedbb_h1=zeros(M*SampleNumberAve,BLK_num);
+    storedbb_h0=zeros(M*SampleNumberAve,1);
+    storedbb_h1=zeros(M*SampleNumberAve,1);
 
-    for pp=1:size(BLK_pow_pool,2)
-        for bb=2:BLK_num            
-            storedbb_h0(:,pp)=storedbb_h0(:,pp)+BLK(:,bb).*CAL(:,bb)*sqrt(BLK_pow_pool(bb,pp));
-        end
-        storedbb_h1(:,pp)=storedbb_h0(:,pp)+BLK(:,1).*CAL(:,1)*sqrt(BLK_pow_pool(1,pp));
-    end
     
+    for bb=2:BLK_num            
+        storedbb_h0=storedbb_h0+BLK(:,bb).*CAL(:,bb)*BLK_amp_pool(bb,runindex);
+    end
+    storedbb_h1=storedbb_h0+BLK(:,1).*CAL(:,1)*BLK_amp_pool(1,runindex);
+
+
     
     %  Tagging begin!
     %  Here we only care about BLK1   
-    for pp=1:size(BLK_pow_pool,2)
-        sig_MNave_BLK(runindex,pp) = correlation_and_pow(BLK(:,1).*CAL(:,1)*sqrt(BLK_pow_pool(1,pp)),CAL(:,1),SampleNumberAve,M);
-        sig_MNave_h0(runindex,pp) = correlation_and_pow(storedbb_h0(:,pp),CAL(:,1),SampleNumberAve,M);
-        sig_MNave_h1(runindex,pp) = correlation_and_pow(storedbb_h1(:,pp),CAL(:,1),SampleNumberAve,M);
-    end
+
+%         BLK_power(runindex)=tagging_v3(BLK(:,1).*CAL(:,1)*BLK_amp_pool(1,runindex),CAL(:,1),SampleNumberAve,1);
+%         h0_power(runindex)=tagging_v3(storedbb_h0,CAL(:,1),SampleNumberAve,1);
+%         h1_power(runindex)=tagging_v3(storedbb_h1,CAL(:,1),SampleNumberAve,1);
+% 
+%     
+        sig_MNave_BLK(runindex)=tagging_v3(BLK(:,1).*CAL(:,1)*BLK_amp_pool(1,runindex),CAL(:,1),SampleNumberAve,M);
+        sig_MNave_h0(runindex)=tagging_v3(storedbb_h0,CAL(:,1),SampleNumberAve,M);
+        sig_MNave_h1(runindex)=tagging_v3(storedbb_h1,CAL(:,1),SampleNumberAve,M);
+
 
 end
 
 %% TH comparison
-TH_pool=linspace(0,2,TH_num)*1e-4;
-tagged_table_h0=zeros(TH_num,runtimes,BLK_num-1);
-tagged_table_h1=zeros(TH_num,runtimes,BLK_num-1);
+TH_num=100;
+TH_pool=linspace(0,15,TH_num)*1e-4;
+tagged_table_h0=zeros(TH_num,runtimes);
+tagged_table_h1=zeros(TH_num,runtimes);
 
-for pp=1:size(BLK_pow_pool,2)
-    for runindex=1:runtimes
-        for TH_index=1:TH_num
-            if sig_MNave_h0(runindex,pp)>TH_pool(TH_index)
-                tagged_table_h0(TH_index,runindex,pp)=1;
-            end
-            if sig_MNave_h1(runindex,pp)>TH_pool(TH_index)
-                tagged_table_h1(TH_index,runindex,pp)=1;
-            end
+
+for runindex=1:runtimes
+    for TH_index=1:TH_num
+        if sig_MNave_h0(runindex)>TH_pool(TH_index)
+            tagged_table_h0(TH_index,runindex)=1;
+        end
+        if sig_MNave_h1(runindex)>TH_pool(TH_index)
+            tagged_table_h1(TH_index,runindex)=1;
         end
     end
+end
 
 
 % ROC generation
 
     for TH_index=1:TH_num
-        Pfa_MC(TH_index,pp)=sum(tagged_table_h0(TH_index,:,pp))/runtimes;
-        Pd_MC(TH_index,pp)=sum(tagged_table_h1(TH_index,:,pp))/runtimes;
+        Pfa_MC(TH_index)=sum(tagged_table_h0(TH_index,:))/runtimes;
+        Pd_MC(TH_index)=sum(tagged_table_h1(TH_index,:))/runtimes;
     end
-end
 
 
-%% plot
+
+% plot
 color=['b';'r';'c';'g';'m'];
 figure(99)
-for pp=1:size(BLK_pow_pool,2)
-plot(Pfa_MC(:,pp),Pd_MC(:,pp),color(pp,:),'linewidth',2);hold on
-end
+
+plot(Pfa_MC,Pd_MC,'linewidth',2);hold on
+
 grid on
 
-if worstcase
-    legend('2 Active BLK','3 Active BLK','4 Active BLK','5 Active BLK','6 Active BLK');
-else
-    legend('2 Active BLK','6 Active BLK','10 Active BLK','14 Active BLK','18 Active BLK');   
-end
-title('-50dBm BLK with -37dBm Interference, M=25')
+
+title('General Case of all BLKs, M=25')
 xlabel('Pfa');
 ylabel('Pd');
-%%
-temp_h0=sort(sig_MNave_h0(:,4));
+%
+temp_h0=sort(sig_MNave_h0);
 
-temp_h1=sort(sig_MNave_h1(:,4));
+temp_h1=sort(sig_MNave_h1);
 
 figure
 subplot(211)
 stem(temp_h0)
 subplot(212)
 stem(temp_h1)
+% 
+% %%
 
-%%
-for ii=1:4
-10*log10(mean(sig_MNave_BLK(:,ii))...
-    /mean(sig_MNave_h0(:,ii)))
-end
+10*log10(mean(sig_MNave_BLK)...
+    /mean(sig_MNave_h0))
 
+% 
